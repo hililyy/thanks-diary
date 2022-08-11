0ffdb33104cc1a8bb2a1f92e9f3899e6b8a1460a
hililyy <joun406@gmail.com>
2022-08-11 22:52:48 +0900

    init

---
+++ Thanks Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
