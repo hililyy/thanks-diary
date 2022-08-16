dbfa1fb8389dca0d99c34d1d02633fa243532918
hililyy <joun406@gmail.com>
2022-08-17 02:56:32 +0900

    feat: 스토리보트 화면 구성

--- Thanks Diary/Splash/View/SplashVC.swift
+++ Thanks Diary/Splash/View/SplashVC.swift
@@ -1,20 +1,16 @@
 //
-//  ViewController.swift
+//  SplashVC.swift
 //  Thanks Diary
 //
 //  Created by 강조은 on 2022/08/11.
 //
 
 import UIKit
-import FSCalendar
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
