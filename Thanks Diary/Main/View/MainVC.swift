81a6b2ccf5f11f26152c45446b491b58b8d2c668
hililyy <joun406@gmail.com>
2022-08-21 23:36:58 +0900

    feat: 캘린더 셋팅

--- Thanks Diary/Main/View/MainVC.swift
+++ Thanks Diary/Main/View/MainVC.swift
@@ -6,13 +6,51 @@
 //
 
 import UIKit
+import FSCalendar
+import Floaty
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setFloty()
+        setCalender()
+    }
+    
+    func setFloty() {
+        let floaty = Floaty()
+        floaty.buttonColor = UIColor(named: "mainColor")!
+        floaty.plusColor = UIColor(named: "whiteColor")!
+        floaty.addItem("자세하게", icon: UIImage(named: "ic_detail_write")!, handler: { item in
+            guard let vc =  self.storyboard?.instantiateViewController(identifier: "WriteVC") as? WriteVC else { return }
+            self.navigationController?.pushViewController(vc, animated: true)
+            floaty.close()
+        })
+        floaty.addItem("간단하게", icon: UIImage(named: "ic_simple_write")!)
+        self.view.addSubview(floaty)
+    }
+    
+    func setCalender() {
+        self.calendar.delegate = self
+        self.calendar.dataSource = self
+        self.calendar.locale = Locale(identifier: "ko_KR")
+        self.calendar.appearance.headerDateFormat = "YYYY년 M월"
+        
+        self.calendar.appearance.headerTitleColor = UIColor(named: "grayColor_1")
+        self.calendar.appearance.headerTitleFont = UIFont(name: "KOTRA_GOTHIC", size: 19)
+        self.calendar.appearance.titleFont = UIFont(name: "KOTRA_GOTHIC", size: 17)
+        self.calendar.appearance.weekdayFont = UIFont(name: "KOTRA_GOTHIC", size: 17)
+        self.calendar.appearance.subtitleFont = UIFont(name: "KOTRA_GOTHIC", size: 17)
+        
+        self.calendar.appearance.weekdayTextColor = UIColor(named: "grayColor_1")
+        self.calendar.appearance.calendar.headerHeight = 70
+        self.calendar.weekdayHeight = 30
+        self.calendar.rowHeight = 50
 
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
@@ -21,4 +59,8 @@ class MainVC: UIViewController {
         guard let vc =  storyboard?.instantiateViewController(identifier: "SettingVC") as? SettingVC else { return }
         self.navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+extension MainVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
 }
---
+++ Thanks Diary/Main/View/WriteVC.swift
@@ -0,0 +1,29 @@
+//
+//  WriteVC.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2022/08/21.
+//
+
+import UIKit
+
+class WriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
