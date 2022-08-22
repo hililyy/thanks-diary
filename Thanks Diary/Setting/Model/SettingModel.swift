3ebb1db0d7c2436f1948f7e23296a2693b2d418e
hililyy <joun406@gmail.com>
2022-08-23 06:03:48 +0900

    edit: MVC구조 파일 추가

---
+++ Thanks Diary/Setting/Model/SettingModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingModel.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2022/08/23.
+//
+
+import Foundation
---
+++ Thanks Diary/Setting/ViewModel/SettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingViewModel.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2022/08/23.
+//
+
+import Foundation
