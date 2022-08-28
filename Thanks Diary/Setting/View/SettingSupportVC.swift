32a46bc17bc0e2c92fa184217c5132b217bb9ff8
hililyy <joun406@gmail.com>
2022-08-28 23:40:24 +0900

    storyboard 수정

---
+++ Thanks Diary/Setting/View/SettingSupportVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingSupportVC.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2022/08/28.
+//
+
+import UIKit
+
+class SettingSupportVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
