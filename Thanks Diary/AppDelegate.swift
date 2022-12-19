1673dc6dd8c024dd2985bc180a707f0f99e214a4
hililyy <joun406@gmail.com>
2022-12-19 23:34:29 +0900

    feat: add firebase

--- Thanks Diary/AppDelegate.swift
+++ Thanks Diary/AppDelegate.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UNUserNotificationCenter.current().delegate = self
+        FirebaseApp.configure()
         return true
     }
 
