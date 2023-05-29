2853cdb49363442c42a62892bd07bda211de07bd
hililyy <joun406@gmail.com>
2023-05-30 01:00:37 +0900

    [FEAT] floating button 코드로 추가

--- thanks-diary/Thanks Diary/Util/Etc/FloatingButton/FloatingButtonCloseView.swift
+++ thanks-diary/Thanks Diary/Util/Etc/FloatingButton/FloatingButtonCloseView.swift
@@ -9,16 +9,67 @@ import UIKit
 
 class FloatingButtonCloseView: UIView {
     
-    private var button: UIButton = {
-        let button = UIButton()
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        return view
+    }()
+    
+    var plusButton: FloatingButton = {
+        let button = FloatingButton()
+        button.setButtonImage(UIImage(named: "add_icon_green") ?? .add)
+        return button
+    }()
+    
+    var button1: FloatingButton = {
+        let button = FloatingButton()
+        button.setButtonImage(UIImage(named: "food1") ?? .remove)
+        return button
+    }()
+    
+    var button2: FloatingButton = {
+        let button = FloatingButton()
+        button.setButtonImage(UIImage(named: "food2") ?? .actions)
         return button
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setConstraints() {
+        addSubview(backgroundView)
+        addSubview(plusButton)
+        addSubview(button1)
+        addSubview(button2)
+        
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            backgroundView.topAnchor.constraint(equalTo: topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            plusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -40),
+            plusButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            plusButton.widthAnchor.constraint(equalToConstant: 40),
+            plusButton.heightAnchor.constraint(equalToConstant: 40),
+            button1.centerXAnchor.constraint(equalTo: plusButton.centerXAnchor),
+            button1.centerYAnchor.constraint(equalTo: plusButton.centerYAnchor, constant: -80),
+            button1.widthAnchor.constraint(equalToConstant: 40),
+            button1.heightAnchor.constraint(equalToConstant: 40),
+            button2.centerXAnchor.constraint(equalTo: plusButton.centerXAnchor),
+            button2.centerYAnchor.constraint(equalTo: plusButton.centerYAnchor, constant: -160),
+            button2.widthAnchor.constraint(equalToConstant: 40),
+            button2.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
 }
--- thanks-diary/Thanks Diary/Util/Etc/FloatingButton/FloatingButtonVC.swift
+++ thanks-diary/Thanks Diary/Util/Etc/FloatingButton/FloatingButtonVC.swift
@@ -8,41 +8,25 @@
 import UIKit
 
 class FloatingButtonVC: BaseVC {
-
-//    @IBOutlet weak var btn1CenterY: NSLayoutConstraint!
-//    @IBOutlet weak var btn2CenterY: NSLayoutConstraint!
-//    @IBOutlet weak var btn3CenterY: NSLayoutConstraint!
+    
+    private let floatingButtonCloseView = FloatingButtonCloseView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        btn1CenterY.constant = 0
-        btn2CenterY.constant = 0
-        btn3CenterY.constant = 0
+        setupViews()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    private func setupViews() {
+        view.addSubview(floatingButtonCloseView)
+        floatingButtonCloseView.translatesAutoresizingMaskIntoConstraints = false
         
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut) {
-            self.btn1CenterY.constant = 80
-            self.btn2CenterY.constant = 160
-            self.btn3CenterY.constant = 240
-            
-            self.view.layoutIfNeeded()  // 작성해주어야 화면 갱신이 됨 (꼭 작성해야함)
-        }
-    }
-    
-    @IBAction func dismissFloating(_ sender: UIButton) {
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut) {
-            self.btn1CenterY.constant = 0
-            self.btn2CenterY.constant = 0
-            self.btn3CenterY.constant = 0
-            
-            self.view.layoutIfNeeded()  // 작성해주어야 화면 갱신이 됨 (꼭 작성해야함)
-        } completion: { completion in
-            // 애니메이션이 끝나는 시점
-            self.dismiss(animated: false)
-        }
+        NSLayoutConstraint.activate([
+            floatingButtonCloseView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            floatingButtonCloseView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            floatingButtonCloseView.topAnchor.constraint(equalTo: view.topAnchor),
+            floatingButtonCloseView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            floatingButtonCloseView.plusButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
+        ])
     }
 }
