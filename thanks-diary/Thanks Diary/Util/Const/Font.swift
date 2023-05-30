08fabce221df7f3fd9d047ca235642a1c711f376
강조은 <joun406@gmail.com>
2023-05-30 18:33:32 +0900

    [ADD] 폰트 파일 추가

---
+++ thanks-diary/Thanks Diary/Util/Const/Font.swift
@@ -0,0 +1,42 @@
+//
+//  Font.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2023/05/30.
+//
+
+import UIKit
+
+class Font {
+    static let NANUM_BOLD_13 = UIFont(name: "NanumBarunGothicBold", size: 13)
+    static let NANUM_BOLD_14 = UIFont(name: "NanumBarunGothicBold", size: 14)
+    static let NANUM_BOLD_15 = UIFont(name: "NanumBarunGothicBold", size: 15)
+    static let NANUM_BOLD_16 = UIFont(name: "NanumBarunGothicBold", size: 16)
+    static let NANUM_BOLD_17 = UIFont(name: "NanumBarunGothicBold", size: 17)
+    static let NANUM_BOLD_18 = UIFont(name: "NanumBarunGothicBold", size: 18)
+    static let NANUM_BOLD_19 = UIFont(name: "NanumBarunGothicBold", size: 19)
+    
+    static let NANUM_13 = UIFont(name: "NanumBarunGothic", size: 13)
+    static let NANUM_14 = UIFont(name: "NanumBarunGothic", size: 14)
+    static let NANUM_15 = UIFont(name: "NanumBarunGothic", size: 15)
+    static let NANUM_16 = UIFont(name: "NanumBarunGothic", size: 16)
+    static let NANUM_17 = UIFont(name: "NanumBarunGothic", size: 17)
+    static let NANUM_18 = UIFont(name: "NanumBarunGothic", size: 18)
+    static let NANUM_19 = UIFont(name: "NanumBarunGothic", size: 19)
+    
+    static let NANUM_LIGHT_13 = UIFont(name: "NanumBarunGothicLight", size: 13)
+    static let NANUM_LIGHT_14 = UIFont(name: "NanumBarunGothicLight", size: 14)
+    static let NANUM_LIGHT_15 = UIFont(name: "NanumBarunGothicLight", size: 15)
+    static let NANUM_LIGHT_16 = UIFont(name: "NanumBarunGothicLight", size: 16)
+    static let NANUM_LIGHT_17 = UIFont(name: "NanumBarunGothicLight", size: 17)
+    static let NANUM_LIGHT_18 = UIFont(name: "NanumBarunGothicLight", size: 18)
+    static let NANUM_LIGHT_19 = UIFont(name: "NanumBarunGothicLight", size: 19)
+    
+    static let NANUM_ULTRALIGHT_13 = UIFont(name: "NanumBarunGothicUltraLight", size: 13)
+    static let NANUM_ULTRALIGHT_14 = UIFont(name: "NanumBarunGothicUltraLight", size: 14)
+    static let NANUM_ULTRALIGHT_15 = UIFont(name: "NanumBarunGothicUltraLight", size: 15)
+    static let NANUM_ULTRALIGHT_16 = UIFont(name: "NanumBarunGothicUltraLight", size: 16)
+    static let NANUM_ULTRALIGHT_17 = UIFont(name: "NanumBarunGothicUltraLight", size: 17)
+    static let NANUM_ULTRALIGHT_18 = UIFont(name: "NanumBarunGothicUltraLight", size: 18)
+    static let NANUM_ULTRALIGHT_19 = UIFont(name: "NanumBarunGothicUltraLight", size: 19)
+}
