f2f60fa086652f3dab63d1857bec45a1f07add4c
강조은 <joun406@gmail.com>
2023-06-26 13:41:29 +0900

    [REFACTOR] UIView Extension autolayout 코드 리팩토링

--- thanks-diary/Thanks Diary/Util/Extension/UIView.swift
+++ thanks-diary/Thanks Diary/Util/Extension/UIView.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 extension UIView {
-    public func setAutoLayout(to:UIView) {
+    public func setAutoLayout(to: UIView) {
         translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.init(item: self, attribute: .leading, relatedBy: .equal, toItem: to, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint.init(item: self, attribute: .trailing, relatedBy: .equal, toItem: to, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint.init(item: self, attribute: .top, relatedBy: .equal, toItem: to, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint.init(item: self, attribute: .bottom, relatedBy: .equal, toItem: to, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-        to.layoutIfNeeded()
+        
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: to.topAnchor),
+            self.leadingAnchor.constraint(equalTo: to.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: to.trailingAnchor),
+            self.bottomAnchor.constraint(equalTo: to.bottomAnchor)
+        ])
     }
 }
