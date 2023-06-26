d877f2f2002a4cc32b520d62ae172a9c9a0d4b4a
강조은 <joun406@gmail.com>
2023-06-26 13:38:11 +0900

    [REFACTOR] Launch 리팩토링

--- thanks-diary/Thanks Diary/Launch/LaunchVC.swift
+++ thanks-diary/Thanks Diary/Launch/LaunchVC.swift
@@ -6,75 +6,17 @@
 //
 
 import UIKit
-import Lottie
-import UserNotifications
 
 class LaunchVC: BaseVC {
     @IBOutlet weak var lottieView: UIView!
-    let userNotificationCenter = UNUserNotificationCenter.current()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let animationView: LottieAnimationView = .init(name: "dot")
-        self.view.addSubview(animationView)
-        animationView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            animationView.topAnchor.constraint(equalTo: lottieView.topAnchor),
-            animationView.bottomAnchor.constraint(equalTo: lottieView.bottomAnchor),
-            animationView.leadingAnchor.constraint(equalTo: lottieView.leadingAnchor),
-            animationView.trailingAnchor.constraint(equalTo: lottieView.trailingAnchor)
-        ])
         
-        animationView.frame = self.lottieView.bounds
-        animationView.center = self.lottieView.center
-        animationView.contentMode = .scaleAspectFit
-        animationView.animationSpeed = 3
-        animationView.play()
-        animationView.loopMode = .loop
+        LottieManager.shared.setLottie(self, lottieView: lottieView, name: "dot", speed: 3, mode: .loop)
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-//            self.requestNotificationAuthorization()
-//            if LocalDataStore.localDataStore.getNewUserData() == true {
-//                if LocalDataStore.localDataStore.getPasswordData() == true {
-//                    self.showPasswordViewController()
-//                } else {
-//                    self.showMainViewController()
-//                }
-//            } else {
-            self.pushVC(name: "Start", identifier: "PageVC")
-//            }
-        }
-    }
-    
-    func showFirstViewController() {
-        let vc = storyboard!.instantiateViewController(withIdentifier: "FirstStartVC") as! FirstStartVC
-        let navi = UINavigationController(rootViewController: vc)
-        navi.modalPresentationStyle = .currentContext
-        present(navi, animated:false, completion: nil)
-    }
-    
-    func showMainViewController() {
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "main")
-        UIApplication.shared.windows.first?.rootViewController = vc
-        UIApplication.shared.windows.first?.makeKeyAndVisible()
-    }
-    
-    func showPasswordViewController() {
-        let vc = storyboard!.instantiateViewController(withIdentifier: "SettingPWVC") as! SettingPWVC
-        vc.homeFlag = true
-        let navi = UINavigationController(rootViewController: vc)
-        navi.modalPresentationStyle = .currentContext
-        present(navi, animated:false, completion: nil)
-    }
-    
-    func requestNotificationAuthorization() {
-        let authOptions = UNAuthorizationOptions(arrayLiteral: .alert, .badge, .sound)
-        
-        userNotificationCenter.requestAuthorization(options: authOptions) { success, error in
-            print("success: \(success)")
-            LocalDataStore.localDataStore.setPushAlarmAgree(newData: success)
-            if let error = error {
-                print("Error: \(error)")
-            }
+            self.setRootVC(name: "Start", identifier: "PageVC")
         }
     }
 }
