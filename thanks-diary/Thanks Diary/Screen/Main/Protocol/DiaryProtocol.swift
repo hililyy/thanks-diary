9f290c042268269b56d930244b8079da0529e82b
강조은 <joun406@gmail.com>
2023-07-18 18:42:03 +0900

    [REFACTOR] 다이어리 CRUD 추상화

--- thanks-diary/Thanks Diary/Screen/Main/Protocol/DiaryProtocol.swift
+++ thanks-diary/Thanks Diary/Screen/Main/Protocol/DiaryProtocol.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
-protocol reloadDelegate: AnyObject {
+protocol DetailDiaryRepository {
+    func setDetailData(title: String, contents: String, completion: @escaping (Bool) -> ())
+    func updateDetailData(selectedIndex: Int, afterTitle: String, afterContents: String, completion: @escaping (Bool) -> ())
+    func deleteDetailData(selectedIndex: Int, completion: @escaping (Bool) -> ())
+    
+}
+
+protocol SimpleDiaryRepository {
+    func setSimpleData(contents: String, completion: @escaping (Bool) -> ())
+    func updateSimpleData(selectedIndex: Int, afterContents: String, completion: @escaping (Bool) -> ())
+    func deleteSimpleData(selectedIndex: Int, completion: @escaping (Bool) -> ())
+}
+
+protocol DiaryReader {
+    func getAllDiaryData(completion: @escaping () -> ())
+    func getSelectedDiaryData(completion: @escaping () -> ())
+}
+
+protocol reloadDelegate {
     func reloadData()
 }
--- thanks-diary/Thanks Diary/Screen/Main/ViewModel/MainViewModel.swift
+++ thanks-diary/Thanks Diary/Screen/Main/ViewModel/MainViewModel.swift
@@ -16,11 +16,27 @@ class MainViewModel {
     
     var selectedDetailData: [DiaryEntity] = []
     var selectedSimpleData: [SimpleDiaryEntity] = []
-    var longDiaryFlag: Bool = false
     var selectedDate: Date = Date()
-    var dateList: [String] = []
     var diaryDates: Set<String> = []
     
+    // 달력에 동그라미칠 날짜 저장
+    func drawCalendarCircle() {
+        diaryDates = []
+        
+        
+        for data in allDetailData {
+            diaryDates.insert(data.key)
+        }
+        
+        for data in allSimpleData {
+            diaryDates.insert(data.key)
+        }
+    }
+}
+
+
+// 다이어리 데이터 가져오기
+extension MainViewModel: DiaryReader {
     // 모든 자세한 일기 데이터 가져오기
     func getAllDiaryData(completion: @escaping () -> ()) {
         allDetailData = CoreDataManager.shared.getDetailData()
@@ -41,20 +57,12 @@ class MainViewModel {
         
         completion()
     }
-    
-    func drawCalendarCircle() {
-        diaryDates = []
-        
-        // 달력에 동그라미칠 날짜 저장
-        for data in allDetailData {
-            diaryDates.insert(data.key)
-        }
-        
-        for data in allSimpleData {
-            diaryDates.insert(data.key)
-        }
-    }
-    
+}
+
+
+// 다이어리 생성, 수정, 삭제 (자세)
+extension MainViewModel: DetailDiaryRepository {
+    // 저장(자세)
     func setDetailData(title: String, contents: String, completion: @escaping (Bool) -> ()) {
         CoreDataManager.shared.setData(
             type: .detail,
@@ -69,19 +77,7 @@ class MainViewModel {
             }
     }
     
-    func setSimpleData(contents: String, completion: @escaping (Bool) -> ()) {
-        CoreDataManager.shared.setData(
-            type: .simple,
-            selectedDate: selectedDate,
-            contents: contents) { result in
-                if result {
-                    completion(true)
-                } else {
-                    completion(false)
-                }
-            }
-    }
-    
+    // 수정(자세)
     func updateDetailData(selectedIndex: Int, afterTitle: String, afterContents: String, completion: @escaping (Bool) -> ()) {
         CoreDataManager.shared.updateData(
             type: .detail,
@@ -98,6 +94,40 @@ class MainViewModel {
             }
     }
     
+    // 삭제(자세)
+    func deleteDetailData(selectedIndex: Int, completion: @escaping (Bool) -> ()) {
+        CoreDataManager.shared.deleteData(
+            type: .detail,
+            selectedDate: selectedDate,
+            title: selectedDetailData[selectedIndex].title ?? "",
+            contents: selectedDetailData[selectedIndex].contents ?? "") { result in
+                if result {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            }
+    }
+}
+
+
+// 다이어리 생성, 수정, 삭제 (단순)
+extension MainViewModel: SimpleDiaryRepository {
+    // 저장(단순)
+    func setSimpleData(contents: String, completion: @escaping (Bool) -> ()) {
+        CoreDataManager.shared.setData(
+            type: .simple,
+            selectedDate: selectedDate,
+            contents: contents) { result in
+                if result {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
+            }
+    }
+    
+    // 수정(단순)
     func updateSimpleData(selectedIndex: Int, afterContents: String, completion: @escaping (Bool) -> ()) {
         CoreDataManager.shared.updateData(
             type: .simple,
@@ -112,20 +142,7 @@ class MainViewModel {
             }
     }
     
-    func deleteDetailData(selectedIndex: Int, completion: @escaping (Bool) -> ()) {
-        CoreDataManager.shared.deleteData(
-            type: .detail,
-            selectedDate: selectedDate,
-            title: selectedDetailData[selectedIndex].title ?? "",
-            contents: selectedDetailData[selectedIndex].contents ?? "") { result in
-                if result {
-                    completion(true)
-                } else {
-                    completion(false)
-                }
-            }
-    }
-    
+    // 삭제(단순)
     func deleteSimpleData(selectedIndex: Int, completion: @escaping (Bool) -> ()) {
         CoreDataManager.shared.deleteData(
             type: .simple,
