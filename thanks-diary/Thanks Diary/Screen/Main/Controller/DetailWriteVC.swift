c6da2194df3dc6a2753ec00311aedf9e52fe0dc4
강조은 <joun406@gmail.com>
2023-07-21 11:54:25 +0900

    [REFACTOR] 일기 자세히 보기 화면 코드로 변경

--- thanks-diary/Thanks Diary/Screen/Main/Controller/DetailWriteVC.swift
+++ thanks-diary/Thanks Diary/Screen/Main/Controller/DetailWriteVC.swift
@@ -9,64 +9,50 @@ import UIKit
 import CoreData
 
 class DetailWriteVC: BaseVC {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var titleTextfield: UITextField!
-    @IBOutlet weak var contentsTextView: UITextView!
-    @IBOutlet weak var completeBtn: UIButton!
     
     var updateFlag: Bool = false
     var parentVC: MainVC?
     var selectedIndex: Int?
+    let detailWriteView = DetailWriteView()
+    
+    override func loadView() {
+        view = detailWriteView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setView()
-        setTitle()
-    }
-    
-    private func setView() {
-        completeBtn.layer.cornerRadius = 10
-        
-        titleTextfield.layer.cornerRadius = 20
-        titleTextfield.layer.borderWidth = 2
-        titleTextfield.layer.borderColor = UIColor(named: "mainColor")?.cgColor
-        titleTextfield.addLeftPadding()
-        
-        contentsTextView.layer.cornerRadius = 20
-        contentsTextView.layer.borderWidth = 2
-        contentsTextView.layer.borderColor = UIColor(named: "mainColor")?.cgColor
-        contentsTextView.textContainerInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 0)
-    }
-    
-    private func setTitle() {
-        if let date = parentVC?.viewModel.selectedDate.convertString(format: "yyyy년 M월 d일") {
-            titleLabel.text = date + " 감사일기"
-        } else {
-            titleLabel.text = "오늘의 감사일기"
-        }
-        
-        if updateFlag == true {
-            guard let index = selectedIndex else { return }
-            
-            titleTextfield.text = parentVC?.viewModel.selectedDetailData[index].title
-            contentsTextView.text = parentVC?.viewModel.selectedDetailData[index].contents
-        }
+        configureUI()
+        setTarget()
     }
 
-    @IBAction func goBack(_ sender: Any) {
-        popVC()
+    private func configureUI() {
+        detailWriteView.setTopLabelData(date: parentVC?.viewModel.selectedDate)
+        
+        if updateFlag == true {
+            guard let index = selectedIndex,
+                  let titleText = parentVC?.viewModel.selectedDetailData[index].title,
+                  let contentsText = parentVC?.viewModel.selectedDetailData[index].contents else { return }
+            
+            detailWriteView.setTextFieldData(titleText: titleText,
+                                             contentsText: contentsText)
+        }
     }
     
-    @IBAction func goComplete(_ sender: Any) {
+    private func setTarget() {
+        detailWriteView.backButton.addTarget {
+            self.popVC()
+        }
         
-        guard let title = titleTextfield.text,
-              let contents = contentsTextView.text else { return }
-        
-        if title.isEmpty || contents.isEmpty {
-            completeBtn.isEnabled = false
+        detailWriteView.completeButton.addTarget {
+            self.complete()
+        }
+    }
+    
+    private func complete() {
+        if detailWriteView.isEmptyTextField() {
+            detailWriteView.setCompleteButtonEnable(isOn: false)
             toast(message: "제목과 내용을 모두 입력해 주세요.", withDuration: 0.5, delay: 1.5) {
-                self.completeBtn.isEnabled = true
+                self.detailWriteView.setCompleteButtonEnable(isOn: true)
             }
             
         } else {
@@ -75,8 +61,8 @@ class DetailWriteVC: BaseVC {
                 
                 parentVC?.viewModel.updateDetailData(
                     selectedIndex: index,
-                    afterTitle: title,
-                    afterContents: contents) { result in
+                    afterTitle: detailWriteView.getTitleText(),
+                    afterContents: detailWriteView.getContentsText()) { result in
                         if result {
                             self.parentVC?.viewModel.getAllDiaryData {
                                 self.parentVC?.viewModel.getSelectedDiaryData {
@@ -88,7 +74,7 @@ class DetailWriteVC: BaseVC {
                         }
                     }
             } else {
-                parentVC?.viewModel.setDetailData(title: title, contents: contents) { result in
+                parentVC?.viewModel.setDetailData(title: detailWriteView.getTitleText(), contents: detailWriteView.getContentsText()) { result in
                     if result {
                         self.popVC()
                     } else {
---
+++ thanks-diary/Thanks Diary/Screen/Main/View/DetailWriteView.swift
@@ -0,0 +1,177 @@
+//
+//  DetailWriteView.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2023/07/21.
+//
+
+import UIKit
+
+final class DetailWriteView: BaseView {
+    
+    var backButton = UIButton(type: .custom).then { button in
+        button.setImage(Image.IC_BACK, for: .normal)
+    }
+    
+    var topLabel = UILabel().then { label in
+        label.font = Font.NANUM_ULTRALIGHT_20
+        label.textColor = Color.COLOR_GRAY1
+    }
+    
+    var completeButton = UIButton(type: .custom).then { button in
+        button.setTitle("완료", for: .normal)
+        button.setTitleColor(Color.COLOR_GRAY1, for: .normal)
+        button.titleLabel?.font = Font.NANUM_LIGHT_15
+        button.backgroundColor = Color.COLOR_LIGHTGRAYBLUE
+        button.layer.cornerRadius = 10
+    }
+    
+    var titleLabel = UILabel().then { label in
+        label.text = "제목"
+        label.font = Font.NANUM_ULTRALIGHT_20
+        label.textColor = Color.COLOR_GRAY1
+    }
+    
+    var contentsLabel = UILabel().then { label in
+        label.text = "내용"
+        label.font = Font.NANUM_ULTRALIGHT_20
+        label.textColor = Color.COLOR_GRAY1
+    }
+    
+    var titleUnderLineImageView = UIImageView().then { imageView in
+        imageView.image = Image.IMG_UNDERLINE
+    }
+    
+    var contentsUnderLineImageView = UIImageView().then { imageView in
+        imageView.image = Image.IMG_UNDERLINE
+    }
+    
+    var titleTextField = UITextField().then { textField in
+        textField.font = Font.NANUM_ULTRALIGHT_17
+        textField.textColor = Color.COLOR_GRAY1
+        textField.layer.cornerRadius = 20
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = Color.COLOR_LIGHTGRAYBLUE?.cgColor
+        textField.addLeftPadding()
+    }
+    
+    var contentsTextView = UITextView().then { textField in
+        textField.font = Font.NANUM_ULTRALIGHT_17
+        textField.textColor = Color.COLOR_GRAY1
+        textField.layer.cornerRadius = 20
+        textField.layer.borderWidth = 2
+        textField.layer.borderColor = Color.COLOR_LIGHTGRAYBLUE?.cgColor
+        textField.textContainerInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 0)
+    }
+    
+    func setTopLabelData(date: Date?) {
+        if let date = date {
+            topLabel.text = date.convertString(format: "yyyy년 M월 d일") + " 감사일기"
+        } else {
+            topLabel.text = "오늘의 감사일기"
+        }
+    }
+    
+    func isEmptyTextField() -> Bool {
+        guard let title = titleTextField.text,
+              let contents = contentsTextView.text else { return false}
+
+        return title.isEmpty || contents.isEmpty ? true : false
+    }
+    
+    func getTitleText() -> String {
+        guard let title = titleTextField.text else { return "" }
+        return title
+    }
+    
+    func getContentsText() -> String {
+        guard let contents = contentsTextView.text else { return "" }
+        return contents
+    }
+    
+    func setCompleteButtonEnable(isOn: Bool) {
+        completeButton.isEnabled = isOn
+    }
+    
+    func setTextFieldData(titleText: String, contentsText: String) {
+        titleTextField.text = titleText
+        contentsTextView.text = contentsText
+    }
+    
+    override func configureUI() {
+        backgroundColor = .white
+    }
+    
+    override func addSubView() {
+        addSubviews([backButton,
+                     topLabel,
+                     completeButton,
+                     titleUnderLineImageView,
+                     titleLabel,
+                     titleTextField,
+                     contentsUnderLineImageView,
+                     contentsLabel,
+                     contentsTextView])
+    }
+    
+    override func setConstraints() {
+        backButton.snp.makeConstraints { make in
+            make.left.equalTo(snp.left).offset(20)
+            make.right.equalTo(topLabel.snp.left).offset(-5)
+            make.width.equalTo(44)
+            make.height.equalTo(44)
+            make.centerY.equalTo(topLabel.snp.centerY)
+        }
+        
+        topLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(25)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.right.equalTo(snp.right).offset(-20)
+            make.width.equalTo(52)
+            make.height.equalTo(40)
+            make.centerY.equalTo(topLabel.snp.centerY)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(backButton.snp.bottom).offset(10)
+            make.left.equalTo(snp.left).offset(35)
+            make.height.equalTo(35)
+        }
+        
+        titleUnderLineImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.top)
+            make.left.equalTo(titleLabel.snp.left).offset(-8)
+            make.bottom.equalTo(titleLabel.snp.bottom)
+            make.width.equalTo(55)
+        }
+        
+        titleTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(3)
+            make.left.equalTo(titleLabel.snp.left)
+            make.centerX.equalTo(snp.centerX)
+            make.bottom.equalTo(contentsLabel.snp.top).offset(-15)
+            make.height.equalTo(44)
+        }
+        
+        contentsLabel.snp.makeConstraints { make in
+            make.left.equalTo(snp.left).offset(35)
+            make.height.equalTo(35)
+        }
+        
+        contentsUnderLineImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentsLabel.snp.top)
+            make.left.equalTo(contentsLabel.snp.left).offset(-8)
+            make.bottom.equalTo(contentsLabel.snp.bottom)
+            make.width.equalTo(55)
+        }
+        
+        contentsTextView.snp.makeConstraints { make in
+            make.top.equalTo(contentsLabel.snp.bottom).offset(3)
+            make.left.equalTo(contentsLabel.snp.left)
+            make.centerX.equalTo(snp.centerX)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-30)
+        }
+    }
+}
