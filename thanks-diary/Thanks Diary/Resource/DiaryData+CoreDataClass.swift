5560e64ed221021ff21d534c2361703f0d676545
hililyy <joun406@gmail.com>
2023-08-22 14:59:16 +0900

    [RENAME] 파일 위치 이동

---
+++ thanks-diary/Thanks Diary/Resource/DiaryData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryData+CoreDataClass.swift
+//  
+//
+//  Created by 강조은 on 2022/09/05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryData)
+public class DiaryData: NSManagedObject {
+
+}
---
+++ thanks-diary/Thanks Diary/Resource/SimpleDiaryData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  SimpleDiaryData+CoreDataClass.swift
+//  
+//
+//  Created by 강조은 on 2022/09/05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SimpleDiaryData)
+public class SimpleDiaryData: NSManagedObject {
+
+}
