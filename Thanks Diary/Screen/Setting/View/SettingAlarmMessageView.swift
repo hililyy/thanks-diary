771c955ad9e779240faac3ed35225928c39876d8
hililyy <joun406@gmail.com>
2023-12-14 15:32:59 +0900

    [FEAT] 푸시 알림 문구 커스텀 기능 추가 #8

---
+++ Thanks Diary/Screen/Setting/View/SettingAlarmMessageView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingAlarmMessageView.swift
+//  Thanks Diary
+//
+//  Created by 강조은 on 2023/12/13.
+//
+
+import UIKit
+import SnapKit
+
+final class SettingAlarmMessageView: BaseView {
+    
+    // MARK: - UI components
+    
+    let navigationView = NavigationView()
+    let iphoneImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    
+}
